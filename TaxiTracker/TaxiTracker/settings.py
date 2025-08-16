82c08ddd25d41317d649328fcec805a986dc7ac3
Voldek404 <iceq3004@yandex.ru>
2025-08-16 21:54:23 +0300

    code:new_app_with_objects

--- TaxiTracker/TaxiTracker/settings.py
+++ TaxiTracker/TaxiTracker/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'vehicles',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'TaxiTracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'diploma',
+        'USER': 'admin',
+        'PASSWORD': '051587',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
---
+++ TaxiTracker/vehicles/models.py
@@ -0,0 +1,14 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
+from django.db.models.fields import IntegerField
+
+
+class Vehicle(models.Model):
+    prod_date = models.DateField()
+    odometer = models.IntegerField(validators=[MaxValueValidator(1000000)])
+    price = models.IntegerField(validators=[MaxValueValidator(1000000)])
+    country_of_origin = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Автомобиль №{self.id}"
