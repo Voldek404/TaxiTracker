83e3f34ef9a01eb4e15455c8777ce0154bde41e7
Voldek404 <iceq3004@yandex.ru>
2025-08-18 12:55:32 +0300

    code:new_model_bran

--- TaxiTracker/vehicles/admin.py
+++ TaxiTracker/vehicles/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
-from .models import Vehicle
+from .models import Vehicle, Brand
 
 # Register your models here.
 admin.site.register(Vehicle)
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'id', 'vehicle_count')
+
+    def vehicle_count(self,obj):
+        return obj.vehicles.count()
+    vehicle_count.short_description = 'Количество машин'
--- TaxiTracker/vehicles/models.py
+++ TaxiTracker/vehicles/models.py
@@ -1,15 +1,31 @@
-from django.core.validators import MaxValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.contrib import admin
 from django.db.models.fields import IntegerField
 
+
+
+
+
+class Brand(models.Model):
+    product_name = models.CharField(max_length=100)
+    car_class = models.CharField(max_length=2)
+    fuel_tank_capacity = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(999)])
+    maximum_load_kg = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(99999)])
+    country_of_origin = models.CharField(max_length=100)
+    number_of_passengers = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(60)])
+
+    def __str__(self):
+        return f"id = {self.id} . Марка авто {self.product_name}"
 
 class Vehicle(models.Model):
     prod_date = models.DateField()
     odometer = models.IntegerField(validators=[MaxValueValidator(1000000)])
-    price = models.IntegerField(validators=[MaxValueValidator(1000000)])
-    country_of_origin = models.CharField(max_length=100)
+    price = models.IntegerField(validators=[MaxValueValidator(10000000)])
     color = models.CharField(max_length=100)
-    plate_number = models.CharField(max_length=9, null = True, blank = True)
+    plate_number = models.CharField(max_length=9, blank = True)
+
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null = True, related_name='vehicles')
 
     def __str__(self):
-        return f"id = {self.id} Авто"
+        return f"id = {self.id} Авто. Госномер {self.plate_number}"
