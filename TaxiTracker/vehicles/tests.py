8d685fcfac3a38ef385d5dc79c0619fe874472fa
Владимир Корнеев <iceq3004@yandex.ru>
2025-09-21 14:02:11 +0300

    test:GET,POST,PUT_tests

--- TaxiTracker/vehicles/tests.py
+++ TaxiTracker/vehicles/tests.py
@@ -1,10 +1,15 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
-from vehicles.models import Vehicle, Brand
+from vehicles.models import Vehicle, Brand, Enterprise, Driver, Manager
 from http import HTTPStatus
 import json
 from django.middleware.csrf import get_token
 from django.http import HttpRequest
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.models import Permission
+
 
 
 class VehiclesCSRFTest(TestCase):
@@ -28,6 +33,10 @@ class VehiclesCSRFTest(TestCase):
             maximum_load_kg=1000,
             number_of_passengers=4
         )
+
+        request = HttpRequest()
+        self.csrf_token = get_token(request)
+        self.client.cookies['csrftoken'] = self.csrf_token
 
     def test_get_csrf_token_directly(self):
         request = HttpRequest()
@@ -68,3 +77,170 @@ class VehiclesCSRFTest(TestCase):
     def test_get_request_works(self):
         response = self.client.get('/api/v1/vehicles/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
+
+
+class PutPostDeleteTestSuperuser(TestCase):
+    def setUp(self):
+        self.client = APIClient()  # DRF APIClient удобнее для JWT
+        User = get_user_model()
+
+        self.user = User.objects.create_superuser(
+            username='superuser',
+            password='12345678asd'
+        )
+        self.brand = Brand.objects.create(
+            product_name='TestBrand',
+            fuel_tank_capacity=50,
+            country_of_origin='TestCountry',
+            car_class='A',
+            maximum_load_kg=1000,
+            number_of_passengers=4
+        )
+
+        self.manager = Manager.objects.create(user=self.user)
+
+        self.enterprise = Enterprise.objects.create(name="TestEnterprise")
+        self.enterprise.managers.set([self.manager])
+
+        self.vehicle = Vehicle.objects.create(
+            plate_number="X000XX178",
+            prod_date="2025-01-01",
+            odometer=0,
+            price=1000000,
+            color="Белый",
+            brand=self.brand,
+            enterprise=self.enterprise
+        )
+
+        response = self.client.post(
+            '/api/v1/token/',
+            {'username': 'superuser', 'password': '12345678asd'},
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+
+    def test_post_creates_vehicle(self):
+        response = self.client.post(
+            '/api/v1/vehicles/',
+            data=json.dumps({
+                "plate_number": "X999XX178",
+                "prod_date": "2025-01-01",
+                "odometer": 0,
+                "price": 1000000,
+                "color": "Синий",
+                "brand": self.brand.id
+            }),
+            content_type='application/json'
+        )
+        print(response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_put_updates_vehicle(self):
+        response = self.client.put(
+            f'/api/v1/vehicles/{self.vehicle.id}/',
+            data=json.dumps({
+                "plate_number": "X111XX178",
+                "prod_date": "2025-01-01",
+                "odometer": 100,
+                "price": 1200000,
+                "color": "Красный",
+                "brand": self.brand.id
+            }),
+            content_type='application/json'
+        )
+        print(response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_removes_vehicle(self):
+        response = self.client.delete(f'/api/v1/vehicles/{self.vehicle.id}/')
+        print(response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class PutPostDeleteTestUser(TestCase):
+    def setUp(self):
+        self.client = APIClient()  # удобнее для JWT
+        User = get_user_model()
+
+        # 1️⃣ Создаём обычного менеджера с is_staff=True
+        self.user = User.objects.create_user(
+            username='user',
+            password='12345678asd',
+            is_staff=True,
+        )
+        vehicle_ct = ContentType.objects.get_for_model(Vehicle)
+        perms = Permission.objects.filter(content_type=vehicle_ct)
+        self.user.user_permissions.set(perms)# 2️⃣ 
+        self.manager = Manager.objects.create(user=self.user)
+
+        self.enterprise = Enterprise.objects.create(name="TestEnterprise", city="TestCity")
+        self.enterprise.managers.set([self.manager])  # ключевой момент
+
+        self.brand = Brand.objects.create(
+            product_name='TestBrand',
+            fuel_tank_capacity=50,
+            country_of_origin='TestCountry',
+            car_class='A',
+            maximum_load_kg=1000,
+            number_of_passengers=4
+        )
+
+        self.vehicle = Vehicle.objects.create(
+            plate_number="X000XX178",
+            prod_date="2025-01-01",
+            odometer=0,
+            price=1000000,
+            color="Белый",
+            brand=self.brand,
+            enterprise=self.enterprise
+        )
+
+        response = self.client.post(
+            '/api/v1/token/',
+            {'username': 'user', 'password': '12345678asd'},
+            format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.token = response.data['access']
+
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+
+    def test_post_creates_vehicle(self):
+        response = self.client.post(
+            '/api/v1/vehicles/',
+            data=json.dumps({
+                "plate_number": "X999XX178",
+                "prod_date": "2025-01-01",
+                "odometer": 0,
+                "price": 1000000,
+                "color": "Синий",
+                "brand": self.brand.id
+            }),
+            content_type='application/json'
+        )
+        print(response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_put_updates_vehicle(self):
+        response = self.client.put(
+            f'/api/v1/vehicles/{self.vehicle.id}/',
+            data=json.dumps({
+                "plate_number": "X111XX178",
+                "prod_date": "2025-01-01",
+                "odometer": 100,
+                "price": 1200000,
+                "color": "Красный",
+                "brand": self.brand.id
+            }),
+            content_type='application/json'
+        )
+        print(response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_removes_vehicle(self):
+        response = self.client.delete(f'/api/v1/vehicles/{self.vehicle.id}/')
+        print(response.status_code)
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
--- TaxiTracker/vehicles/views.py
+++ TaxiTracker/vehicles/views.py
@@ -7,18 +7,25 @@ from django.views.decorators.csrf import csrf_protect
 from django.utils.decorators import method_decorator
 from rest_framework.authentication import BasicAuthentication
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import PermissionDenied
+from rest_framework.permissions import DjangoModelPermissions,DjangoObjectPermissions, IsAuthenticated
+from rest_framework.exceptions import PermissionDenied, APIException
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden, HttpResponseBadRequest
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from rest_framework import status
 
 
+
+class ConflictError(APIException):
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = "Запрещенная операция"
+    default_code = "conflict"
+
+
 class VehiclesApiView(generics.ListCreateAPIView):
     queryset = Vehicle.objects.all()
     serializer_class = VehiclesSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [DjangoModelPermissions]
     authentication_classes = [JWTAuthentication]
 
     def handle_exception(self, exc):
@@ -28,8 +35,11 @@ class VehiclesApiView(generics.ListCreateAPIView):
         return response
 
     def perform_create(self, serializer):
-        user = self.request.user
-        serializer.save(enterprise=user.managers.enterprises.first())
+        try:
+            manager = Manager.objects.get(user=self.request.user)
+            serializer.save(enterprise=manager.enterprises.first())
+        except Manager.DoesNotExist:
+            raise PermissionDenied("У вас нет прав на создание автомобиля")
 
     def dispatch(self, *args, **kwargs):
         return super().dispatch(*args, **kwargs)
@@ -54,18 +64,20 @@ class VehiclesDetailApiView(generics.RetrieveUpdateDestroyAPIView):
             obj = Vehicle._base_manager.get(pk=pk)
         except Vehicle.DoesNotExist:
             raise Http404
+
         if hasattr(user, 'managers'):
             if not user.managers.enterprises.filter(pk=obj.enterprise_id).exists():
                 raise PermissionDenied("У вас нет прав на просмотр этой машины")
-        else:
-            raise PermissionDenied("У вас нет прав на просмотр")
+        if self.request.method == "DELETE"  and not user.is_superuser:
+            raise ConflictError()
+
         return obj
 
 
 class BrandsApiView(generics.ListCreateAPIView):
     queryset = Brand.objects.all()
     serializer_class = BrandsSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [DjangoModelPermissions]
     authentication_classes = [JWTAuthentication]
 
     def get_queryset(self):
@@ -78,6 +90,8 @@ class BrandsApiView(generics.ListCreateAPIView):
 class DriversApiView(generics.ListCreateAPIView):
     queryset = Driver.objects.all()
     serializer_class = DriversSerializer
+    permission_classes = [DjangoModelPermissions]
+    authentication_classes = [JWTAuthentication]
 
     def get_queryset(self):
         user = self.request.user
@@ -89,7 +103,7 @@ class DriversApiView(generics.ListCreateAPIView):
 class EnterprisesApiView(generics.ListCreateAPIView):
     queryset = Enterprise.objects.all()
     serializer_class = EnterprisesSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [DjangoModelPermissions]
     authentication_classes = [JWTAuthentication]
 
     def perform_create(self, serializer):
@@ -135,7 +149,7 @@ class EnterprisesDetailApiView(generics.RetrieveUpdateDestroyAPIView):
 class ManagersApiView(generics.ListCreateAPIView):
     queryset = Manager.objects.all()
     serializer_class = ManagersSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [DjangoModelPermissions]
     authentication_classes = [JWTAuthentication]
 
 
