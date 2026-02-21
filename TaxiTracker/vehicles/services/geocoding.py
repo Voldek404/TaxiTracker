b7c8f7e0fa703a4bc2dbc02762f51f3463c95009
Владимир Корнеев <iceq3004@yandex.ru>
2026-02-21 13:55:45 +0300

    code:extra_geoapi_done

--- TaxiTracker/vehicles/services/geocoding.py
+++ TaxiTracker/vehicles/services/geocoding.py
@@ -3,19 +3,23 @@ from django.conf import settings
 
 API_KEY = "9d21bd0b-f7f2-4438-9643-0ae8a5807b52"
 BASE_URL = "https://graphhopper.com/api/1"
+API_KEY_2= '9e61f84ec90f419193bae5004f179977'
+SECOND_URL = "https://api.geoapify.com/v1/geocode/reverse"
 
 
 def reverse_geocode(lat, lon):
-    url = f"{BASE_URL}/geocode"
-    params = {
-        "reverse": "true",
-        "point": f"{lat},{lon}",
-        "key": API_KEY,
-    }
-
-    r = requests.get(url, params=params, timeout=10)
-    r.raise_for_status()
-    data = r.json()
+    try:
+        url = f"{BASE_URL}/geocode"
+        params = {
+            "reverse": "true",
+            "point": f"{lat},{lon}",
+            "key": API_KEY,
+        }
+        r = requests.get(url, params=params, timeout=10)
+        r.raise_for_status()
+        data = r.json()
+    except requests.RequestException as e:
+        return reverse_geocode_second(lat, lon)
 
     if "hits" in data and len(data["hits"]) > 0:
         hit = data["hits"][0]
@@ -37,13 +41,30 @@ def reverse_geocode(lat, lon):
         # Добавляем город, если нет ничего выше
         if not parts and hit.get("city"):
             parts.append(hit["city"])
-
-        # Можно добавить почтовый индекс
         if hit.get("postalcode"):
             parts.append(hit["postalcode"])
-
-        # Склеиваем через запятую
         address = ", ".join(parts)
         return address
 
     return None
+
+def reverse_geocode_second(lat, lon):
+    try:
+        params = {
+            "lat": lat,
+            "lon": lon,
+            "apiKey": API_KEY_2,
+        }
+
+        r = requests.get(SECOND_URL, params=params, timeout=10)
+        r.raise_for_status()
+        data = r.json()
+
+        features = data.get("features")
+        if not features:
+            return None
+
+        return features[0]["properties"].get("formatted")
+
+    except requests.RequestException:
+        return None
